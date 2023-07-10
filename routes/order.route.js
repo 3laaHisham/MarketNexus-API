e075b4afd24e7fb6219e0ca8b56c470d15844da1
Abdulrahman Fahmy <abdulrhmanabotor@gmail.com>
2023-07-10 15:49:04 +0300

    order + review routers are done

--- routes/order.route.js
+++ routes/order.route.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const controller = require("../controllers");
 
-// const { method1, method2 } = orderController;
+const { isAuthenticated, isAuthorized } = require("../middlewares");
 
-// router.route("/:id").post(protect, method1).get(protect, method2);
+router.post("/", isAuthenticated, (req, res) => controller(res)(getOrderById)(req.body.order));
+router.get("/", isAuthenticated, (req, res) => controller(res)(getAllOrders)());
+
+
+router.get("/:id", isAuthenticated, (req, res) => controller(res)(getOrderById)(req.params.id));
+//the order of the next functions is important 
+router.put("/:id/cancel", isAuthenticated, (req, res) => controller(res)(cancelOrder)(req.params.id));
+router.put("/:id/:status", isAuthorized("admin"), (req, res) => controller(res)(updateOrderStatus)(req.params.id, req.params.status));
 
 module.exports = router;
--- routes/review.route.js
+++ routes/review.route.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const controller = require("../controllers");
 
-// const { method1, method2 } = reviewController;
+const { isAuthenticated, isAuthorized } = require("../middlewares");
+router.get("/:id", (req, res) =>
+    controller(res)(getReviewById)(req.params.id));
+router.put(":/id", isAuthenticated, (req, res) => {
+    controller(res)(updateReviewById)(req.params.id, req.body.message, body.stars);
+});
+router.delete("/:id", isAuthenticated, (req, res) => {
+    controller(res)(deleteReview)(req.params.id);
+});
 
-// router.route("/:id").post(protect, method1).get(protect, method2);
+// router.get("/product/:productID", (req, res) => controller(res)()(req.params.productID));
+//TODO
+
+router.post(":/productID", isAuthenticated, (req, res) => controller(res, createNewreview)());
+//TODO 
+
 
 module.exports = router;
